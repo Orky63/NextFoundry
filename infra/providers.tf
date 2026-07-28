ab57ed9d7f54532001ff39e5ace36c67550989ab
Orky63 <131435557+Orky63@users.noreply.github.com>
2026-07-28 12:12:19 +0100

    Configure remote Terraform backend

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,16 +1,6 @@
 terraform {
   required_version = ">= 1.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-
-  terraform {
-  required_version = ">= 1.0"
-
   backend "s3" {
     bucket = "nextfoundry-terraform-state-387344700059"
     key    = "infra/terraform.tfstate"
